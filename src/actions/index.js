73260fccd8824f8e339cc1648452d8a8ed2f1b41
j2FunOnly <j2funonly@ya.ru>
2019-01-22 16:04:12 +0300

    Users can sign in

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,6 +14,18 @@ export const signup = (formProps, callback) => async dispatch => {
   }
 };
 
+export const signin = (formProps, callback) => async dispatch => {
+  try {
+    const response = await axios.post('http://localhost:8080/signin', formProps);
+
+    dispatch({ type: AUTH_USER, payload: response.data.token });
+    localStorage.setItem('token', response.data.token);
+    callback();
+  } catch (e) {
+    dispatch({ type: AUTH_ERROR, payload: 'Email or password incorrect!' });
+  }
+};
+
 export const signout = () => {
   localStorage.removeItem('token');
 
